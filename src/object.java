b7aaa0602562a965d1ee72ccd55f8e18da5d2e49
RYKER0905 <btech10155.22@bitmesra.ac.in>
2023-09-15 19:49:15 +0530

    assign 1

---
+++ src/object.java
@@ -0,0 +1,16 @@
+public class object {
+    // Method in MyClass
+    public void displayMessage() {
+        System.out.println("Hello, this is a method from MyClass.");
+    }
+
+    public static void main(String[] args) {
+        // Create an object of MyClass
+        object obj = new object();
+
+        // Call the displayMessage method using the object
+        obj.displayMessage();
+    }
+}
+
+
