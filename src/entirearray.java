9a802d5ea5230e006e903d6b256baf400f9b6e2d
RYKER0905 <btech10155.22@bitmesra.ac.in>
2023-09-16 11:54:49 +0530

    assign 5 print all array

---
+++ src/entirearray.java
@@ -0,0 +1,9 @@
+public class entirearray {
+    public static void main(String[] args){
+        int result[] = {90,81,97,68,81,77};
+        for (int i=0;i<5;i++){
+            System.out.println(result[i]);
+        }
+
+    }
+}
